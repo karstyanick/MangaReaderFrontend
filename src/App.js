64f5d64c712beb04956e26493febb0a474e9ae74
Yanick Karst <yanick.karst@the-roommate.com>
2022-08-07 22:35:57 +0200

    multiuser visual

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { useState, useRef, useEffect  } from "react";
 import axios from "axios";
 import ImageGallery from 'react-image-gallery';
 import "../node_modules/react-image-gallery/styles/css/image-gallery.css"
-import {AddMangaModal, ReadMangaModal} from "./Modal"
+import {AddMangaModal, ReadMangaModal, SignupModal} from "./Modal"
 import plusmanga from "./plusmanga.png"
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 
-let BACKENDHOST = "http://95.179.132.168"
-//let BACKENDHOST = "http://localhost:5000"
+axios.defaults.withCredentials = true
+
+//let BACKENDHOST = "https://mangareaderbackend.lol"
+let BACKENDHOST = "http://localhost:5000"
 
 const imgStyles = {
   height:"300px",
   margin:"10px",
   "borderRadius": "10px"
 };
+
+const authButtonStyles = {
+  width: "100px",
+  "min-height": "30px",
+  padding: "0"
+}
 
 const navChaptersRight = {
   height:"40px",
@@ -42,9 +51,11 @@ function App() {
   let imageGallery;
 
 
-  let wakeLock = null
   const addMangaRef = useRef()
   const addMangaChaptersRef = useRef()
+
+  const usernameRef = useRef()
+  const passwordRef = useRef()
 
   const pagePreviousRef = useRef({});
   const isOpenReadMangaPreviousRef = useRef(false)
@@ -56,11 +67,15 @@ function App() {
   const [manga, setManga] = useState([])
   const [currentManga, setCurrentManga] = useState("")
   const [currentPage, setCurrentPage] = useState({})
+  const [isOpenSignup, setOpenSignup] = useState(false)
   const [isOpenAddManga, setOpenAddManga] = useState(false)
   const [isOpenReadManga, setOpenReadManga] = useState(false)
   const [visible, setVisible] = useState(false);
+  const [visibleLogin, setVisibleLogin] = useState(false)
+  const [visibleLogout, setVisibleLogout] = useState(true)
   const [lastPage, setlastPage] = useState(false)
   const [showIndex, toggleShowIndex] = useState(false)
+  const [currentUser, setCurrentUser] = useState("")
 
   useEffect(() => {
     initPage()
@@ -79,9 +94,6 @@ function App() {
 
   function saveState(currentPage, chapter, currentManga) {
 
-    console.log(`currentPage: ${JSON.stringify(currentPage)}, currentChapter: ${JSON.stringify(chapter)}, currentManga: ${currentManga}`)
-
-
     if(JSON.stringify(currentPage) !== JSON.stringify(pagePreviousRef.current)){
       axios.post(`${BACKENDHOST}/save`, {
         "name": currentManga,
@@ -94,21 +106,77 @@ function App() {
     }
   }
 
+  async function signup(){
+    const username = usernameRef.current.value
+    const password = passwordRef.current.value
+
+    return axios.post(`${BACKENDHOST}/signup`, {
+        "username": username,
+        "password": password
+    }).then(response => {
+        firstPageLoadRef.current = true
+        initPage()
+        setCurrentUser(response.data)
+        setOpenSignup(false)
+    })
+  }
+
+  async function signin(){
+    const username = usernameRef.current.value
+    const password = passwordRef.current.value
+
+    return axios.post(`${BACKENDHOST}/login`, {
+        "username": username,
+        "password": password
+    }).then(response => {
+        if(response.data !== "Wrong password"){
+          firstPageLoadRef.current = true
+          initPage()
+          setCurrentUser(response.data)
+          setOpenSignup(false)
+        }
+    })
+  }
+
+  async function logout(){
+    return axios.post(`${BACKENDHOST}/logout`,{
+    }).then(response => {
+      firstPageLoadRef.current = true
+      setCurrentUser("")
+      initPage()
+    })
+  }
+
   async function initPage(){
     if(firstPageLoadRef.current === true){
       firstPageLoadRef.current = false
       return axios.get(`${BACKENDHOST}/`, {
-      }).then(response => {
-        console.log(JSON.stringify(response.data))
-        addManga(response.data.posters.concat(mangas))
-        if(response.data.state.currentChapter){
-          setChapter(response.data.state.currentChapter)
-          setCurrentChapterNumber(response.data.state.currentChapterNumber)
-        }
-        if(response.data.state.currentPage){
-          setCurrentPage(response.data.state.currentPage)
-          pagePreviousRef.current = response.data.state.currentPage
-        }
+      })
+      .then(response => {
+          addManga(response.data.posters.concat(mangas))
+          if(response.data.state.currentChapter){
+            setChapter(response.data.state.currentChapter)
+            setCurrentChapterNumber(response.data.state.currentChapterNumber)
+          }
+          if(response.data.state.currentPage){
+            setCurrentPage(response.data.state.currentPage)
+            pagePreviousRef.current = response.data.state.currentPage
+          }
+          setCurrentUser(response.data.username)
+          setVisibleLogin(false)
+          setVisibleLogout(true)
+      })
+      .catch( error => {
+          setChapter({})
+          setCurrentChapterNumber({})
+          addManga([{id: "addManga", name:"addManga", poster: plusmanga}])
+          setManga([])
+          setCurrentManga("")
+          setCurrentPage({})
+          setlastPage(false)
+          toggleShowIndex(false)
+          setVisibleLogin(true)
+          setVisibleLogout(false)
       })
     }
   }
@@ -125,7 +193,6 @@ function App() {
         mangas.pop()
         addManga([...mangas, response.data.metaData, {id: "addManga", name:"addManga", poster: plusmanga}])
       }
-      console.log(mangas)
     })
   }
 
@@ -160,7 +227,6 @@ function App() {
         "chapter": chapterToGet
       }
     }).then(response => {
-      console.log(response.data)
       setChapter({...chapter, [mangaName]: response.data.links})
       setCurrentChapterNumber({...chapterNumber, [mangaName]: response.data.chapter})
       setlastPage(false)
@@ -175,16 +241,16 @@ function App() {
   }
 
   async function openReadManga(name){
+    console.log("called")
     if(name === "addManga"){
       setOpenAddManga(true)
     }else{
-      console.log(mangas)
       setCurrentManga(name)
       if(!chapter[name]){
         await setChapter({...chapter, [name]: []})
       }
       
-      wakeLock = await navigator.wakeLock.request();
+      await navigator.wakeLock.request();
 
       const manga = await getManga(name)
       setManga(manga)
@@ -199,7 +265,6 @@ function App() {
   function checkLastPage(index){
     if(index===chapter[currentManga].length-1){
       setlastPage(true)
-      console.log(chapter)
     }else{
       setlastPage(false)
     }
@@ -208,35 +273,51 @@ function App() {
   return (
     <>
 
-    <div class="posters">{mangas.map(manga => <img style = {imgStyles} onClick={() => openReadManga(manga.name)} alt={""} key={manga.id} name={manga.name} src={manga.poster}/>)}</div>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{display: "flex", flexDirection: "row"}}>
+        {visibleLogin && 
+          <button class="button-31" style={{...authButtonStyles}} onClick={() => setOpenSignup(true)} onTouchCancel={() => setOpenSignup(true)}>Sing in</button>
+        }
+        <span style={{ fontSize: "30px", color: "white" }}>{currentUser}</span>
+        {visibleLogout &&
+          <button class="button-31" style={{...authButtonStyles, right: "10px", position:"absolute"}} onClick={() => logout()} onTouchCancel={() => setOpenSignup(true)}>Log out</button>
+        }
+      </div>
+      <div class="posters">{mangas.map(manga => <img style = {imgStyles} onClick={() => openReadManga(manga.name)} onTouchCancel={() => openReadManga(manga.name)} alt={""} key={manga.id} name={manga.name} src={manga.poster}/>)}</div>
+    </div>
+
+    
 
     <AddMangaModal addManga={addNewManga} open={isOpenAddManga} onClose={() => setOpenAddManga(false)}>
-      Add Manga
-      <input ref={addMangaRef}></input>
-      Chapters
-      <input ref={addMangaChaptersRef}></input>
+      <input ref={addMangaRef} placeholder={"Manga"}></input>
+      <input ref={addMangaChaptersRef} placeholder={"ChapterRange (a-b)"}></input>
     </AddMangaModal>
+
+    <SignupModal signup={signup} signin={signin} open={isOpenSignup} onClose={() => setOpenSignup(false)}>
+      <input ref={usernameRef} placeholder={"Username"}></input>
+      <input ref={passwordRef} placeholder={"Password"}></input>
+    </SignupModal>
 
     <ReadMangaModal open={isOpenReadManga} onClose={() => onCloseModal()} setVisible={()=> setVisible(!visible)}>
       {lastPage &&<>
-        <button class="button-31" style={navChaptersRight} onClick={() => getChapter(currentManga, parseInt(chapterNumber[currentManga])-1)}>previous</button>
+        <button class="button-31" style={navChaptersRight} onClick={() => getChapter(currentManga, parseInt(chapterNumber[currentManga])-1)} onTouchCancel = {() => getChapter(currentManga, parseInt(chapterNumber[currentManga])-1)}>previous</button>
         </>
       }
       <div>
-      <ImageGallery lazyLoad={true} ref={i => imageGallery = i} showIndex={showIndex} onClick={()=> toggleShowIndex(!showIndex)} onSlide={index => checkLastPage(index)} items={chapter[currentManga]} isRTL={true} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} showNav={false} slideDuration={300}></ImageGallery>
+      <ImageGallery lazyLoad={true} ref={i => imageGallery = i} showIndex={showIndex} onClick={()=> toggleShowIndex(!showIndex)} onTouchCancel = {()=> toggleShowIndex(!showIndex)} onSlide={index => checkLastPage(index)} items={chapter[currentManga]} isRTL={true} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} showNav={false} slideDuration={300}></ImageGallery>
       </div>
       <div class = "chaptersWrapper">
       <span style = {{color:"white", "margin-right": "50px"}}>{currentManga}</span>
       <div class = "chapters">
       {manga.map(chapter => {
         const color = (chapter === chapterNumber[currentManga]) ? "#ffa07a" : ""
-        return <button style={{backgroundColor:color}}class="button-31" key={chapter} onClick={() => getChapter(currentManga, chapter)}>{chapter}</button>      
+        return <button style={{backgroundColor:color}}class="button-31" key={chapter} onClick={() => getChapter(currentManga, chapter)} onTouchCancel={() => getChapter(currentManga, chapter)}>{chapter}</button>      
       })}
-      <button class="button-31" key={"addChapters"} onClick={() => addChapters()}>Add 10 Chapters </button>
+      <button class="button-31" key={"addChapters"} onClick={() => addChapters()} onTouchCancel={() => addChapters()}>Add 10 Chapters </button>
       </div>
       </div>
       {lastPage &&<>
-        <button class="button-31" style={navChaptersLeft} onClick={() => getChapter(currentManga, parseInt(chapterNumber[currentManga])+1)}>next</button>
+        <button class="button-31" style={navChaptersLeft} onClick={() => getChapter(currentManga, parseInt(chapterNumber[currentManga])+1)} onTouchCancel={() => getChapter(currentManga, parseInt(chapterNumber[currentManga])+1)}>next</button>
         </>
       }
 
@@ -247,9 +328,9 @@ function App() {
           <div class = "chapters">
           {manga.map(chapter => {
             const color = (chapter === chapterNumber[currentManga]) ? "#ffa07a" : ""
-            return <button style={{backgroundColor:color}}class="button-31" key={chapter} onClick={() => getChapter(currentManga, chapter)}>{chapter}</button>      
+            return <button style={{backgroundColor:color}}class="button-31" key={chapter} onClick={() => getChapter(currentManga, chapter)} onTouchCancel={() => getChapter(currentManga, chapter)}>{chapter}</button>      
           })}
-          <button class="button-31" key={"addChapters"} onClick={() => addChapters()}>Add 10 Chapters </button>  
+          <button class="button-31" key={"addChapters"} onClick={() => addChapters()} onTouchCancel={() => addChapters()}>Add 10 Chapters </button>  
           </div>
           </div>
       </>}
--- src/Modal.js
+++ src/Modal.js
@@ -4,8 +4,12 @@ import ReactDOM from 'react-dom'
 const MODAL_STYLES_1 = {
     position: "fixed",
     top: "10px",
-    padding: "50px",
-    zIndex: 1000
+    zIndex: 1000,
+    display: "flex",
+    "flex-direction": "column",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
 }
 
 const MODAL_STYLES_2 = {
@@ -20,6 +24,14 @@ const MODAL_STYLES_2 = {
     right: "0px",
     "max-height": "100%",
     "align-content": "center"
+}
+
+const CLOSE_STYLES = {
+    position: "fixed", 
+    right:"10px", 
+    top:"10px", 
+    width: "100px", 
+    zIndex: "1000"
 }
 
 const OVERLAY_STYLES = {
@@ -41,10 +53,10 @@ export function AddMangaModal({addManga, open, children, onClose}) {
     return ReactDOM.createPortal(
         <>
             <div style={OVERLAY_STYLES}/>
+            <button class={"button-31"} style={CLOSE_STYLES} onClick={onClose}>Close</button>
             <div style={MODAL_STYLES_1}>
-                <button onClick={onClose}>Close Modal</button>
                 {children}
-                <button onClick={addManga}>Confirm</button>
+                <button class={"button-31"} style={{"margin-top": "5px"}} onClick={addManga}>Confirm</button>
             </div>
         </>,
         document.getElementById("portal")
@@ -62,6 +74,23 @@ export function ReadMangaModal({children, open, onClose, setVisible}){
             </div>
             <button class = "button-31 hiddenbutton" onClick={setVisible}>=</button>
             <button class={"button-31"} style={{position: "fixed", zIndex: 1000, right: "0%", top:"0%", width: "0px", "minHeight": "0px", "minWidth": "0px", height:"38px", "fontSize": "small", display: "flex", "justifyContent": "center"}} onClick={onClose} >X</button>
+        </>,
+        document.getElementById("portal")
+    )
+}
+
+export function SignupModal({signup, signin, children, open, onClose}){
+    if(!open) return null
+
+    return ReactDOM.createPortal(
+        <>
+            <div style={OVERLAY_STYLES}/>
+            <button class={"button-31"} style={CLOSE_STYLES} onClick={onClose}>Close</button>
+            <div style={MODAL_STYLES_1}>
+                {children}
+                <button class={"button-31"} onClick={signup}>Signup</button>
+                <button class={"button-31"} onClick={signin}>Singin</button>
+            </div>
         </>,
         document.getElementById("portal")
     )
